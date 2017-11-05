286d4f1c6384df53859bacda0ef257177b63cf5e
git <zq@qq.com>
2017-11-05 20:55:15 +0800

    调整后台报错问题

--- common/config/params.php
+++ common/config/params.php
@@ -3,6 +3,9 @@
  * @TODO 配置网站的固定信息
  * @TIME 2017-03-12
  */
+
+require(__DIR__ . '/../../common/const/Const.php');
+
 return [
     'adminEmail' => 'admin@example.com',
     'supportEmail' => 'support@example.com',
