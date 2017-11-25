0b45e73a2184287e9df20946b777ffd2958934c1
git <zq@qq.com>
2017-11-25 19:07:13 +0800

    添加退出功能

--- backend/controllers/LoginController.php
+++ backend/controllers/LoginController.php
@@ -6,29 +6,43 @@ use backend\modules\system\models\LoginForm;
 use Yii;
 use yii\web\Response;
 
-class LoginController extends BaseController
-{
-    public $hasTop = false;
-    public $hasFoot = false;
-    public $layout = false;
+class LoginController extends BaseController {
 
-    public function actionLogin()
-    {
-    	if(\Yii::$app->user->identity){
-    	    //如果已经登录 跳转首页
-		    $this->redirect('/');
-	    }
+	public $hasTop = false;
+	public $hasFoot = false;
+	public $layout = false;
 
-        $model = new LoginForm();
-        if (Yii::$app->request->isAjax) {
-            Yii::$app->response->format = Response::FORMAT_JSON;
-            if ($model->load(['LoginForm' => Yii::$app->request->post(), '']) && $model->login()) {
-                return [ 'code' => 200, 'msg' => '登陆成功', 'url' => '/'];
-            } else {
-                return ['code' => 0, 'msg' => '用户名或密码不正确'];
-            }
-        }
+	public function actionLogin()
+	{
+		if( \Yii::$app->user->identity ) {
+			//如果已经登录 跳转首页
+			$this->redirect( '/' );
+		}
+		$model = new LoginForm();
+		if( Yii::$app->request->isAjax ) {
+			Yii::$app->response->format = Response::FORMAT_JSON;
+			if( $model->load( [ 'LoginForm' => Yii::$app->request->post(), '' ] ) && $model->login() ) {
+				return [ 'code' => 200, 'msg' => '登陆成功', 'url' => '/' ];
+			} else {
+				return [ 'code' => 0, 'msg' => '用户名或密码不正确' ];
+			}
+		}
 
-        return $this->render('index', ['model' => $model]);
-    }
+		return $this->render( 'index', [ 'model' => $model ] );
+	}
+
+	/**
+	 * 退出登录
+	 *
+	 * @return Response
+	 */
+	public function actionLoginOut()
+	{
+		if (Yii::$app->user->isGuest) {
+			return $this->goHome();
+		} else {
+			Yii::$app->user->logout();
+			return $this->redirect('/login/login');
+		}
+	}
 }
--- backend/views/layouts/main.php
+++ backend/views/layouts/main.php
@@ -66,40 +66,23 @@ $this->registerJsFile( '/public/plugins/laypage/laypage.js' );
                     <li class="dropdown user user-menu">
                         <a href="#" class="dropdown-toggle" data-toggle="dropdown">
                             <img src="/public/dist/img/user2-160x160.jpg" class="user-image" alt="User Image">
-                            <span class="hidden-xs">Alexander Pierce</span>
+                            <span class="hidden-xs"><?=\Yii::$app->user->getIdentity()->username?></span>
                         </a>
                         <ul class="dropdown-menu">
                             <!-- User image -->
                             <li class="user-header">
                                 <img src="/public/dist/img/user2-160x160.jpg" class="img-circle" alt="User Image">
-
                                 <p>
-                                    Alexander Pierce - Web Developer
-                                    <small>Member since Nov. 2012</small>
+                                    <?=\Yii::$app->user->getIdentity()->username?>
                                 </p>
-                            </li>
-                            <!-- Menu Body -->
-                            <li class="user-body">
-                                <div class="row">
-                                    <div class="col-xs-4 text-center">
-                                        <a href="#">Followers</a>
-                                    </div>
-                                    <div class="col-xs-4 text-center">
-                                        <a href="#">Sales</a>
-                                    </div>
-                                    <div class="col-xs-4 text-center">
-                                        <a href="#">Friends</a>
-                                    </div>
-                                </div>
-                                <!-- /.row -->
                             </li>
                             <!-- Menu Footer-->
                             <li class="user-footer">
                                 <div class="pull-left">
-                                    <a href="#" class="btn btn-default btn-flat">Profile</a>
+                                    <a href="javascript:;" class="btn btn-default btn-flat">Profile</a>
                                 </div>
                                 <div class="pull-right">
-                                    <a href="#" class="btn btn-default btn-flat">Sign out</a>
+                                    <a href="/login/login-out" class="btn btn-default btn-flat">Sign out</a>
                                 </div>
                             </li>
                         </ul>
