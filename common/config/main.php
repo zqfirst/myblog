789269b328767a838aec2c11452b50fe89280267
git <zq@qq.com>
2017-11-26 21:34:53 +0800

    兼容处理

--- common/config/main.php
+++ common/config/main.php
@@ -1,18 +1,25 @@
 <?php
-return [
-    'vendorPath' => dirname(dirname(__DIR__)) . '/vendor',
-    'language' => 'zh-CN',
-    'sourceLanguage' => 'zh-CN',
-    'components' => [
-        'cache' => [
-            'class' => 'yii\caching\FileCache',
-        ],
-        'urlManager' => [
-            'enablePrettyUrl' => true,
-            'showScriptName' => false,
-        ],
-        'devicedetect' => [
-	        'class' => 'alexandernst\devicedetect\DeviceDetect'
-        ],
-    ],
+$params = [
+	'vendorPath'     => dirname( dirname( __DIR__ ) ) . '/vendor',
+	'language'       => 'zh-CN',
+	'sourceLanguage' => 'zh-CN',
+	'components'     => [
+		'cache'        => [
+			'class' => 'yii\caching\FileCache',
+		],
+		'urlManager'   => [
+			'enablePrettyUrl' => true,
+			'showScriptName'  => false,
+		],
+	],
 ];
+
+if( class_exists( 'alexandernst\devicedetect\DeviceDetect' ) ) {
+	$params['components'] = array_merge( $params['components'], [
+		'devicedetect' => [
+			'class' => 'alexandernst\devicedetect\DeviceDetect'
+		]
+	] );
+}
+
+return $params;
