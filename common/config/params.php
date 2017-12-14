ffaa9e35e0b9730be59c7f7dcd5c2e8080720fd8
zhangqiang <zhangqiang@wangcaigu.com>
2017-12-14 16:09:41 +0800

    微信公众号私钥修改

--- common/config/params.php
+++ common/config/params.php
@@ -11,9 +11,9 @@ return [
     'adminEmail'                    => 'admin@example.com',
     'supportEmail'                  => 'support@example.com',
     'wechat_config'                 => [
-        'app_id'      => 'wx435e8e396d1654d5',
+        'app_id'     => 'wx435e8e396d1654d5',
         'token'      => '63d53045d928baacfaee7d4e2a9d9cfa',
         'aes_key'    => 'OsdGhIvf85kh2oBm4MrNHuufW3NaSqDE2k1YVDSWfoz',
-        'app_secret' => 'eEOOWMpejIfede15rLe2IOuOfh0SGmtT',
+        'app_secret' => '927cd66eccbf1c4082921456aad5db93',
     ],
 ];
